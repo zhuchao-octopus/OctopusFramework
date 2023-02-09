f1c2b5386d7e8a426322fc092f2f8dec2483ee34
m <m@zhuchao.top>
2023-02-09 15:03:55 +0800

    update uart

--- libJOSUtils/src/main/java/com/zhuchao/android/serialport/TUartFile.java
+++ libJOSUtils/src/main/java/com/zhuchao/android/serialport/TUartFile.java
@@ -89,7 +89,7 @@ public class TUartFile extends TDevice implements TCourierEventListener {
         if (deviceReadingEventListener != null) {//处理一帧数据
             TProtocol_Package tProtocol_package = new TProtocol_Package();
             tProtocol_package.parse(eventCourier.getDatas());
-            if(tProtocol_package.getMsg_End() == tProtocol_package.DEFAULT_END &&
+            if(tProtocol_package.getMsgHead() == tProtocol_package.DEFAULT_HEAD &&
                     tProtocol_package.getMsg_End() == tProtocol_package.DEFAULT_END)
             eventCourier.setObj(tProtocol_package);
 
